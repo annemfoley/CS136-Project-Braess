62e9e6d96ecca680f7fe638dc3470ae16915cf87
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-12-05 14:07:09 -0500

    put equilibrium check back in sim

--- sim.py
+++ sim.py
@@ -15,7 +15,7 @@ def main():
     network.addEdge(network.source,b,0,25) # cost 25
     network.addEdge(a,network.sink,0,25) # cost 25
     network.addEdge(b,network.sink,0.01,0) # cost x/100
-    #network.addEdge(a,b,0,0) # cost 0
+    network.addEdge(a,b,0,0) # cost 0
 
 
     # test validity of network and display
@@ -35,14 +35,13 @@ def main():
     for i in range(max_iters):
         equilibrium = True # whether current round is in equilibrium
         for agent in agents:
-            agent.setBestPath()
-        #     if agent.updateAndCheckChanged(): # if returns true, then we are not in equilibrium
-        #         equilibrium = False
-        # if equilibrium: # no need to continue if we're in equilibrium
-        #     print("Reached Equilibrium! Iteration:",i)
-        #     break
-        # if i==max_iters-1:
-        #     print("Reached max iters, not equilibrium.")
+            if agent.updateAndCheckChanged(): # if returns true, then we are not in equilibrium
+                equilibrium = False
+        if equilibrium: # no need to continue if we're in equilibrium
+            print("Reached Equilibrium! Iteration:",i)
+            break
+        if i==max_iters-1:
+            print("Reached max iters, not equilibrium.")
 
     # display the new state of the network:
     network.displayNetwork()
