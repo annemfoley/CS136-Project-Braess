13ad19142bf023dccd9dbe110cae492d721b22be
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-12-05 13:20:14 -0500

    created simulation

--- agent.py
+++ agent.py
@@ -1,38 +1,40 @@
 from network import Network
 
 class Agent:
-    def __init__(self):
+    def __init__(self,network,id=''):
+        self.id = id
         self.path = []
         self.cost = float('inf')
+        self.network = network
 
     # find the path that would result in lowest cost and take it
-    def setBestPath(self,network):
+    def setBestPath(self):
 
         # "reset" ourself to the source
         #   i.e. if we are currently on a path, take ourself off
         if len(self.path) > 0:
-            network.subtractOneFromPath(self.path)
+            self.network.subtractOneFromPath(self.path)
 
-        best_path = network.paths[0]
+        best_path = self.network.paths[0]
         best_cost = float('inf')
         # find the best path to add self to
-        for p in network.paths:
-            network.addOneToPath(p) # simulate going along that path
-            cost = network.getCostAlongPath(p)
+        for p in self.network.paths:
+            self.network.addOneToPath(p) # simulate going along that path
+            cost = self.network.getCostAlongPath(p)
             if  cost < best_cost:
                 best_path = p
                 best_cost = cost
-            network.subtractOneFromPath(p)  # reset at the source again
+            self.network.subtractOneFromPath(p)  # reset at the source again
 
         # now add ourselves to the best path
-        network.addOneToPath(best_path)
+        self.network.addOneToPath(best_path)
 
         self.path = best_path
         self.cost = best_cost
     
-    
+
     # set agent on best path and check if their path choice changed
-    def updateAndCheckChanged(self):
+    def updateAndCheckChanged(self,debug=False):
         old_path = self.path
         self.setBestPath()
         if old_path == self.path:
