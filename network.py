90cc34731b9fbb979b98d756dd3392b09fe33d84
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-12-05 11:38:18 -0500

    Initial commit

---
+++ network.py
@@ -0,0 +1,139 @@
+# import dictionary for graph
+from collections import defaultdict
+
+######## NOTE: idk if we need a vertex class or can just represent them as strings
+class Vertex:
+    def __init__(self,string,source=False,sink=False):
+        self.string = string # for display purposes
+        self.source = source # is this vertex a source?, default false
+        self.sink = sink # is this vertex a sink?, default false
+
+
+class Network:
+
+    def __init__(self):
+        self.graph = defaultdict(list) # Array list of all edges, edge (u,v) is a string pair
+        self.source = Vertex('s',True,False) # network source
+        self.sink = Vertex('s',False,True) # network sink
+        self.vertices = [self.source,self.sink] # list of existing vertices
+
+        self.costs = dict() # Cost along edge, stored (alpha,beta) pairs where cost is calculated alpha*x+beta
+        self.flows = dict() # Flow along any edge, i.e. the number of agents taking that edge
+
+
+    # Create new edge with cost, cost=0 by default and flow starts at 0
+    def addEdge(self,u,v,alpha=0,beta=0):
+        if u.sink:
+            print("Cannot add edge from the sink")
+            return
+        if v.source:
+            print("Cannot add an edge to the source")
+            return
+        if u not in self.vertices:
+            self.vertices.append(u)
+        if v not in self.vertices:
+            self.vertices.append(v)
+        self.graph[u].append(v)
+        self.costs[(u,v)]=(alpha,beta)
+        self.flows[(u,v)]=0
+
+    # Return a list of all vertices that are on directed edge from vertex u
+    def getNextVertices(self,u):
+        return self.graph[u]
+
+    # get all edges in the network
+    def generate_edges(self):
+        edges = []
+        # for each node in graph
+        for node in self.graph:
+            # for each neighbour node of a single node
+            for neighbour in self.graph[node]:
+                # if edge exists then append
+                edges.append((node, neighbour))
+        return edges
+
+    # Set the cost for an edge
+    def setCost(self,u,v,alpha,beta):
+        if v in self.graph[u]:
+            self.costs[(u,v)]=(alpha,beta)
+        else:
+            print("No edge",(u,v))
+
+    # Set the flow along an edge
+    def setFlow(self,u,v,x):
+        if v in self.graph[u]:
+            self.costs[(u,v)]=x
+        else:
+            print("No edge",(u,v))
+
+    # Increments flow on an edge by 1
+    def incrementFlow(self,u,v):
+        self.setFlow(u,v,self.flows[(u,v)]+1)
+
+    # Decrements flow on an edge by 1
+    def decrementFlow(self,u,v):
+        self.setFlow(u,v,self.flows[(u,v)]-1)
+
+    # Calculate the cost to a single agent from edge (u,v)
+    def getEdgeCost(self,u,v):
+        # return alpha * x + beta
+        return self.costs[(u,v)][0] * self.flows[(u,v)] + self.costs[(u,v)][1]
+
+    # Calculate total cost of current congestion
+    def calcTotalCost(self,u,v):
+        # return sum over edges (edge cost * flow along edge)
+        cost = 0
+        edges = self.generate_edges()
+        for (u,v) in edges:
+            cost += self.getEdgeCost(u,v) * self.flows[(u,v)]
+        return cost
+
+
+    # Use BFS to make sure this is a valid network
+    #   i.e. all vertices should point to the sink
+    # idk if this actually works, from geeksforgeeks
+    def checkNetwork(self):
+
+        # Check source and sink are actually a source and sink
+        for v in self.vertices:
+            if self.source in self.getNextVertices(v):
+                print("Cannot have vertex pointing to source")
+                return False
+        if self.getNextVertices(self.sink):
+            print("Cannot have the sink point to another vertex")
+            return False
+
+
+        # Mark all the vertices as not visited
+        visited = dict.fromkeys(self.vertices,False)
+       
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(self.source)
+        visited[self.source] = True
+ 
+        while queue:
+ 
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+
+            # if this vertex has no more edges, check it's a sink
+            if len(self.getNextVertices(s))==0:
+                if not s.sink:
+                    print("Cannot end on non-sink")
+                    return False
+ 
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.getNextVertices(s):
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+        return True
