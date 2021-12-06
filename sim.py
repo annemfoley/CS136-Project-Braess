e30aa2408c4857f65b8318b3a3064a62dd42e1b4
Nikita Jindal <nikitajindal@dhcp-10-250-159-142.harvard.edu>
2021-12-06 12:21:26 -0500

    Finished simulations

--- sim.py
+++ sim.py
@@ -6,30 +6,73 @@ import numpy as np
 
 # simulate agents finding equilibrium on a network
 def main():
-    flowWithoutHighway  = []
-    flowWithHighway = []
+    route1Flow  = []
+    route3Flow = []
     totalCostWithHighway = []
-    totalCostWithoutHighway = []    
-    for i in range(1, 2001):
-        flow1, flow3, cost = run_simulation(i, False)
-        totalCostWithoutHighway.append(cost)
-    for i in range(1, 2001):
-        flow1, flow3, cost = run_simulation(i)
-        flowWithoutHighway.append(flow1)
-        flowWithHighway.append(flow3)
+    totalCostWithoutHighway = []
+    totalCostWithHighwayFixedDemand = []
+    optimalCost = []
+    highwayCostAtConvergence = []
+   
+    highwayCost = list(range(1, 21))
+    demand = list(range(1, 601))
+
+    for i in range(1, 1001):
+        # Record Optimal cost for different demands
+        flow1_, flow3_, cost_ = run_simulation(i, False)
+        totalCostWithoutHighway.append(cost_)
+
+        # Record Total Cost and Flow on Routes w Highway for different demands
+        flow1, flow3, cost = run_simulation(i, True, 7.5)
+        route1Flow.append(flow1)
+        route3Flow.append(flow3)
         totalCostWithHighway.append(cost)
-    x = list(range(1, 2001))
-    plt.figure(0)
-    plt.plot(x, flowWithoutHighway, label = "Flow on routes 1 and 2")
-    plt.plot(x, flowWithHighway, label = "Flow on route 3")
-    plt.legend()
-    plt.figure(1)
-    plt.plot(x, totalCostWithHighway, label = "Total cost with highway")
-    plt.plot(x, totalCostWithoutHighway, label = "Total cost without highway")
-    plt.legend()
+    
+    # Convergence of total cost to optimal, can change hard coded demand 
+    for i in highwayCost: 
+        flow1_, flow3_, cost_ = run_simulation(50, True, i)
+        totalCostWithHighwayFixedDemand.append(cost_)
+        flow1, flow3, cost = run_simulation(50)
+        optimalCost.append(cost)
+
+    # For a particular demand value, at what highway cost does the total cost of the 5-link network
+    # converge with the optimal cost 
+
+    for d in demand:
+        seen = False
+        for i in highwayCost:
+            flow_1, flow_3, cost_ = run_simulation(d, True, i)
+            flow1, flow3, cost = run_simulation(d)
+            if cost == cost_ and not seen:
+                highwayCostAtConvergence.append(i)
+                seen = True
+
+    
+    x = list(range(1, 1001))
+    plot2(0, "Flow on Different Paths for 5-Link Network", "Demand", "Flow On Path", x, route1Flow, x, route3Flow, "Flow on routes 1 and 2", "Flow on route 3")
+    plot2(1, "Total Cost with and without Highway", "Demand", "Total Cost", x, totalCostWithHighway, x, totalCostWithoutHighway, "Total cost with highway", "Total cost without highway")
+    plot2(2, "Total cost with and without highway", "Highway Cost", "Total Cost", highwayCost, totalCostWithHighwayFixedDemand, highwayCost, optimalCost, "Total cost of 5-link network for demand = 50", "Optimal cost for demand = 50")
+    plot(3, "Highway cost and Convergence towards Optimal cost", "Demand", "Cost of Highway", demand, highwayCostAtConvergence, "Cost of Highway where Total cost of 5-link first converges with Optimal cost")
     plt.show()
 
-def run_simulation(demand, highway = True):
+def plot(figure, title, xlabel, ylabel, xdata, ydata, label):
+    plt.figure(figure)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(xdata, ydata, label = label)
+    plt.legend()
+
+def plot2(figure, title, xlabel, ylabel, xdata, ydata, x2data, y2data, label, label2):
+    plt.figure(figure)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(xdata, ydata, label = label)
+    plt.plot(x2data, y2data, label = label2)
+    plt.legend()
+
+def run_simulation(demand, highway = False, highwayCost = 7.5):
     max_iters = 20 # should turn into command line argument
     num_agents = demand
 
@@ -37,17 +80,12 @@ def run_simulation(demand, highway = True):
     a = Vertex('a')
     b = Vertex('b')
 
-    # network.addEdge(network.source,a,0,0.01) # cost x/100
-    # network.addEdge(network.source,b,15,0) # cost 15
-    # network.addEdge(a,network.sink,15,0) # cost 15
-    # network.addEdge(b,network.sink,0,0.01) # cost x/100
-    network.addEdge(network.source,a,0,0.01) # 
-    network.addEdge(network.source,b,15, 0) # 
-    network.addEdge(a,network.sink,15,0) # 
-    network.addEdge(b,network.sink,0,0.01) # 
+    network.addEdge(network.source, a, 0, 0.01) # 
+    network.addEdge(network.source, b, 15, 0) # 
+    network.addEdge(a,network.sink, 15, 0) # 
+    network.addEdge(b,network.sink, 0, 0.01) # 
     if highway:
-        network.addEdge(a,b,7.5,0) # cost 0
-        # network.addEdge(a,b,0,0) # cost 0
+        network.addEdge(a,b,highwayCost,0) # cost 0
 
     # test validity of network and display
     network.checkNetwork(debug=True)
